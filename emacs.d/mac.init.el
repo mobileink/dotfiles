3d963e83158d4f56b75aa615566f2ace4a4e3096
Gregg Reynolds <reynolds-gregg@norc.org>
2018-12-20 10:05:15 -0600

    mac.init.el changes
    Signed-off-by: Gregg Reynolds <reynolds-gregg@norc.org>

--- emacs.d/mac.init.el
+++ emacs.d/mac.init.el
@@ -1,13 +1,21 @@
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
-  (add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t))
+;و;(when (>= emacs-major-version 24)
+(require 'package)
+(package-initialize)
+;;(add-to-list 'package-archives '("gnu" . "http://elpa.gnu.org/packages/") t)
+;;(add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+;;(add-to-list 'package-archives '("marmalade" . "http://marmalade-repo.org/packages/") t)
+(add-to-list 'package-pinned-packages '(cider . "melpa-stable") t)
+;;  )
+
+(when (not package-archive-contents)
+  (package-refresh-contents))
+
 
 (when (memq window-system '(mac ns))
 ;; NB: the shell path is the same one you get opening a shell, i.e. in the case of bash it runs ~/.bash_profile etc.
-(exec-path-from-shell-initialize)
+  (message "window-system:" window-system)
+  (exec-path-from-shell-initialize)
 )
 
 ;; (setq exec-path
@@ -26,8 +34,9 @@
 ;; (message (getenv "PATH"))
 
 (add-to-list 'load-path "~/.emacs.d/elisp")
+;; (load "my-abbrevs")
 
-(add-to-list 'load-path "~/.emacs.d/ProofGeneral/generic/")
+;; (add-to-list 'load-path "~/.emacs.d/ProofGeneral/generic/")
 
 ;; (load "~/.emacs.d/init.el")
 (custom-set-variables
@@ -35,21 +44,17 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(c-basic-offset 4)
+ '(change-log-default-name "CHANGELOG.md")
+ '(cider-boot-parameters "")
  '(latex-run-command "lualatex")
+ '(magit-commit-arguments (quote ("--signoff")))
+ '(package-selected-packages
+   (quote
+    (ggtags helm cider dart-mode yas-jit xcscope solarized-theme smartparens rainbow-delimiters paredit mustache-mode markdown-mode magit ido-vertical-mode ido-ubiquitous ido-at-point groovy-mode gradle-mode exec-path-from-shell ess edn ctags company-math color-theme-solarized color-theme-sanityinc-solarized clojurescript-mode clojure-snippets clojure-quick-repls auto-complete-nxml adoc-mode)))
  '(rng-schema-locating-files
    (quote
-    ("schemas.xml" "/Applications/Emacs.app/Contents/Resources/etc/schema/schemas.xml" "/usr/local/xml/schema/dita/schemas.xml")))
- '(safe-local-variable-values
-   (quote
-    ((warroot . "/Users/gar/sparky/sparky-rest/target/sparky-1.0-SNAPSHOT/WEB-INF")
-     (srcroot . "/Users/gar/sparky/sparky-rest/src/main/clojure")
-     (app . "sparky-rest")
-     (warroot . "/Users/gar/sparky/hello/target/hello-1.0-SNAPSHOT/WEB-INF/classes")
-     (srcroot . "/Users/gar/sparky/hello/src/main/clojure")
-     (warroot . "/Users/gar/dev/aama-rest/war/WEB-INF/classes")
-     (warroot . "/Users/gar/dev/aama-rest/war")
-     (srcroot . "/Users/gar/dev/aama-rest/src")
-     (app . "aama")))))
+    ("schemas.xml" "/Applications/Emacs.app/Contents/Resources/etc/schema/schemas.xml" "/usr/local/xml/schema/dita/schemas.xml"))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -85,6 +90,14 @@
 			     (blink-matching-open))))
     (when matching-text (message matching-text))))
 
+;; Align with spaces only
+(setq-default indent-tabs-mode nil)
+(defadvice align-regexp (around align-regexp-with-spaces)
+  "Never use tabs for alignment."
+  (let ((indent-tabs-mode nil))
+    ad-do-it))
+(ad-activate 'align-regexp)
+
 (setq-default show-trailing-whitespace t)
 (setq transient-mark-mode t)
 (global-hl-line-mode 1)
@@ -93,7 +106,7 @@
 (global-font-lock-mode 1)
 
 (setq user-full-name "Gregg Reynolds")
-(setq user-mail-address "gar@mobileink.com")
+;(setq user-mail-address "gar@mobileink.com")
 
 (defalias 'qrr 'query-replace-regexp)
 
@@ -119,6 +132,32 @@
 ;;                                            'fullboth)))
 ;; (global-set-key [(meta return)] 'toggle-fullscreen)
 
+;; (require 'xcscope)
+;; (cscope-setup)
+
+(setq-default c-basic-offset 4)
+
+(require 'recentf)
+
+;; enable recent files mode.
+(recentf-mode t)
+;; 50 files ought to be enough.
+(setq recentf-max-saved-items 50)
+
+(defun ido-recentf-open ()
+  "Use `ido-completing-read' to \\[find-file] a recent file"
+  (interactive)
+  (if (find-file (ido-completing-read "Find recent file: " recentf-list))
+      (message "Opening file...")
+        (message "Aborting")))
+
+;; magit
+(global-set-key (kbd "C-c g") 'magit-status)
+
+;; get rid of `find-file-read-only' and replace it with something
+;; more useful.
+(global-set-key (kbd "C-x C-r") 'ido-recentf-open)
+
 (global-set-key (kbd "C-c h") 'helm-mini)
 
 (global-set-key (kbd "C-x C-f") 'helm-find-files)
@@ -127,7 +166,7 @@
 (if (> emacs-major-version 21)
     (progn
       (set-language-environment "utf-8")
-      (set-input-method "latin-1-postfix")
+      ;; (set-input-method "latin-1-postfix")
       (prefer-coding-system 'utf-8)
       (setq locale-coding-system 'utf-8)
       (set-terminal-coding-system 'utf-8)
@@ -159,13 +198,13 @@
        '((left . 100)
 	 (top . 50)
 	 (height . 50)
-	 (width . 175)))
+	 (width . 215)))
 
 (setq default-frame-alist
        '((left . 100)
 	 (top . 50)
 	 (height . 50)
-	 (width . 175)))
+	 (width . 215)))
 
 ;;(autoload 'toggle-show-trailing-whitespace "utils" "utils" t)
 (defun toggle-show-trailing-whitespace ()
@@ -189,15 +228,25 @@
   (uniquify-region-lines (point-min) (point-max)))
 
 
+ (add-hook 'c-mode-common-hook
+	   (lambda ()
+	     (when (derived-mode-p 'c-mode 'c++-mode 'java-mode)
+	       (ggtags-mode 1))))
+
+(add-hook 'c-mode-hook (lambda () comment-style (quote multi-line)))
+
 ;; (load-theme 'solarized-dark t)
-(load-theme 'solarized-light t)
+;; (load-theme 'solarized-light t)
+;; (color-theme-solarized-dark)
 
 ;; http://www.emacswiki.org/emacs/RainbowDelimiters
-(require 'rainbow-delimiters)
+;;(require 'rainbow-delimiters)
 ;; (add-hook 'clojure-mode-hook 'rainbow-delimiters-mode)
 ;; (add-hook 'prog-mode-hook 'rainbow-delimiters-mode)
-(global-rainbow-delimiters-mode)
+;; (global-rainbow-delimiters-mode)
 
+(require 'tramp)
+(setq tramp-default-method "ssh")
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
@@ -209,7 +258,7 @@
 (defalias 'list-buffers 'ibuffer)
 ;; make buffer switch command show suggestions
 ;; http://ergoemacs.org/emacs/emacs_buffer_switching.html
-(require 'ido)
+;(require 'ido)
 (ido-mode t)
 ;; http://ergoemacs.org/emacs/elisp_next_prev_user_buffer.html
 ;;;;;todo ...
@@ -260,22 +309,38 @@
 (setq reftex-plug-into-AUCTeX t)
 
 
-;;(require 'smartparens-config)
+;; (require 'smartparens-config)
 
 ;;  Clojure
 ;;(require 'clojure-mode)
 
-(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
-(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
+;;(add-to-list 'auto-mode-alist '("\\.cljs$" . clojure-mode))
+;;(add-to-list 'auto-mode-alist '("\\.edn$" . clojure-mode))
 
-(load (expand-file-name "~/.emacs.d/elisp/migae"))
-(add-hook 'clojure-mode-hook
-      (lambda ()
-        (set (make-local-variable 'parse-sexp-ignore-comments) t) ))
-(add-hook 'clojure-mode-hook
-	  (lambda ()
-	    (message "clojure-mode-hook migae: %s" (buffer-file-name))
-	    (define-key clojure-mode-map (kbd "C-x C-s") 'migae-save-clj-buffer)))
+;; (load (expand-file-name "~/.emacs.d/elisp/migae"))
+;; (add-hook 'clojure-mode-hook
+;;       (lambda ()
+;;         (set (make-local-variable 'parse-sexp-ignore-comments) t) ))
+;; (add-hook 'clojure-mode-hook
+;; 	  (lambda ()
+;; 	    (message "clojure-mode-hook migae: %s" (buffer-file-name))
+;; 	    (define-key clojure-mode-map (kbd "C-x C-s") 'migae-save-clj-buffer)))
+
+;; (add-hook 'js-mode-hook
+;; 	  (lambda ()
+;; 	    (message "js-mode-hook migae: %s" (buffer-file-name))
+;; 	    (define-key js-mode-map (kbd "C-x C-s") 'migae-save-js-buffer)))
+
+;; (add-hook 'nxml-mode-hook
+;; 	  (lambda ()
+;; 	    (message "nxml-mode-hook migae: %s" (buffer-file-name))
+;; 	    (define-key nxml-mode-map (kbd "C-x C-s") 'migae-save-nxml-buffer)))
+
+;; (add-hook 'html-mode-hook
+;; 	  (lambda ()
+;; 	    (message "html-mode-hook migae: %s" (buffer-file-name))
+;; 	    (define-key html-mode-map (kbd "C-x C-s") 'migae-save-html-buffer)))
+
 ;;(add-hook 'ruby-mode-hook (lambda () (local-set-key 'f1 'ri)))
 
 ;; (define-clojure-indent
@@ -289,17 +354,40 @@
 ;;   (context 2))
 
 ;;
-;(add-hook 'cider-mode-hook 'cider-turn-on-eldoc-mode)
-;(add-hook 'cider-repl-mode-hook 'paredit-mode)
-;(add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
-;(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
+(add-hook 'cider-mode-hook 'turn-on-eldoc-mode)
+;;(add-hook 'cider-repl-mode-hook 'paredit-mode)
+(add-hook 'cider-repl-mode-hook 'smartparens-strict-mode)
+(add-hook 'cider-repl-mode-hook 'rainbow-delimiters-mode)
 ;(setq nrepl-hide-special-buffers t)
 
-
+(add-to-list 'load-path "~/.emacs.d/snippets")
 (require 'yasnippet)
 (yas-global-mode 1)
 
-(require 'ess-site)
+;; ;; Remove Yasnippet's default tab key binding
+;; (define-key yas-minor-mode-map (kbd "<tab>") nil)
+;; (define-key yas-minor-mode-map (kbd "TAB") nil)
+;; ;; Set Yasnippet's key binding to shift+tab
+;; (define-key yas-minor-mode-map (kbd "<backtab>") 'yas-expand)
+;; ;; Alternatively use Control-c + tab
+;; (define-key yas-minor-mode-map (kbd "\C-c TAB") 'yas-expand)
+
+;; (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
+;; (ac-config-default)
+;; (global-auto-complete-mode t)
+;; (ac-set-trigger-key "TAB")
+;; (ac-set-trigger-key "<tab>")
+
+;;(add-to-list 'load-path "~/.emacs.d/elisp/doc-mode-1.1")
+(add-to-list 'auto-mode-alist '("\\.adoc$" . adoc-mode))
+(autoload 'adoc-mode "adoc-mode")
+(add-hook 'adoc-mode-hook (lambda() (buffer-face-mode f)))
+
+;; bazel
+(add-to-list 'auto-mode-alist '("BUILD$" . python-mode))
+
+
+;(require 'ess-site)
 
 ;;
 ;; ESS julia language
@@ -312,6 +400,8 @@
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;   nxml schemata
+
+(add-to-list 'auto-mode-alist '("\\.html$" . nxml-mode))
 
 (defun set-nxml-schemas ()
   (setq rng-schema-locating-files-default
@@ -326,6 +416,10 @@
 
 (add-hook 'nxml-mode-hook 'set-nxml-schemas)
 
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;;  dita
+(add-to-list 'auto-mode-alist '("\\.ditamap$" . nxml-mode))
+(add-to-list 'auto-mode-alist '("\\.dita$" . nxml-mode))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;   netrexx
@@ -348,12 +442,12 @@
 (load "mykeys")
 
 ;; (load-file "dir/generic/proof-site.el")
-(require 'proof-site)
+;;(require 'proof-site)
 
-(setenv "ATSHOME" "/usr/local/lib/ats2-postiats-0.1.2")
-(setenv "PATSHOME" "/usr/local/lib/ats2-postiats-0.1.2")
+;; (setenv "ATSHOME" "/usr/local/lib/ats2-postiats-0.1.2")
+;; (setenv "PATSHOME" "/usr/local/lib/ats2-postiats-0.1.2")
 
-(require 'ats2-flymake)
+;; (require 'ats2-flymake)
 
 
 (set-face-attribute 'default nil :foundry "apple" :family "Source Code Pro")
@@ -362,7 +456,32 @@
 (setq command-line-default-directory "~/")
 (setq inhibit-splash-screen t)
 
-(add-to-list 'auto-mode-alist '("\\.g4\\'" . antlr-mode))
+;; (add-to-list 'auto-mode-alist '("\\.g4\\'" . antlr-mode))
 
-(load-file (let ((coding-system-for-read 'utf-8))
-                (shell-command-to-string "agda-mode locate")))
+;; (load-file (let ((coding-system-for-read 'utf-8))
+;;                 (shell-command-to-string "agda-mode locate")))
+
+(when (eq system-type 'darwin)
+  (message "system-type: darwin")
+
+  ;; default Latin font (e.g. Consolas)
+  (set-face-attribute 'default t :family "Monaco")
+
+  (set-default-font "Monaco")
+
+  ;; default font size (point * 10)
+  ;;
+  ;; WARNING!  Depending on the default font,
+  ;; if the size is not supported very well, the frame will be clipped
+  ;; so that the beginning of the buffer may not be visible correctly. 
+  (set-face-attribute 'default nil :height 135)
+
+  ;; use specific font for Korean charset.
+  ;; if you want to use different font size for specific charset,
+  ;; add :size POINT-SIZE in the font-spec.
+  ;; (set-fontset-font t 'hangul (font-spec :name "NanumGothicCoding"))
+
+  ;; you may want to add different for other charset in this way.
+  )
+
+(split-window-right)
